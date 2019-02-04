710ebd59de8420c42f9b2e857e71e5711498ed8b
Bojan Okretic <bojan@Bojans-MBP.lan>
2019-02-04 14:44:11 +0100

    Navbar default settings

--- generators/app/templates/src/js/addons/offcanvas.js
+++ generators/app/templates/src/js/addons/offcanvas.js
@@ -1,19 +1,14 @@
-(function ($, Drupal, window, document, undefined) {
+(function ($, document) {
 
   // To understand behaviors, see https://drupal.org/node/756722#behaviors
-  Drupal.behaviors.<%= themeName %> = {
-    attach: function(context, settings) {
-
-      // Place your code here.
-      $(document).ready(function() {
-        if ($('body').hasClass('body-offcanvas')) {
-          $('.navbar-toggler').on('click', function () {
-            $('body').toggleClass('overflow-hidden');
-          })
-        }
-      });
-
+   // Place your code here.
+   $(document).ready(function() {
+    if ($('body').hasClass('body-offcanvas')) {
+      $('.navbar-toggler').on('click', function () {
+        $('body').toggleClass('overflow-hidden');
+        $('.dialog-off-canvas-main-canvas').toggleClass('overflow-hidden');
+      })
     }
-  };
+  });
 
 })(jQuery, Drupal, this, this.document);
